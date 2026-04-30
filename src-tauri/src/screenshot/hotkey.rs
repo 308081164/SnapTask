aab208bce23bcf9c8f3a38011bc16abb5829df19
Hui_Laoding <125714211+308081164@users.noreply.github.com>
2026-05-01 01:50:04 +0800

    fix: add ShortcutState import and fix callback signatures for Tauri 2.x global-shortcut API

--- src-tauri/src/screenshot/hotkey.rs
+++ src-tauri/src/screenshot/hotkey.rs
@@ -3,14 +3,14 @@ use tauri::{
     Emitter,
     Manager,
 };
-use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut};
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
 use log::{info, error};
 
 /// 注册所有全局热键
 pub fn register_hotkeys(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
     // Ctrl+Shift+S: 选区截屏
     let area_shortcut = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyS);
-    if let Err(e) = app.global_shortcut().on_shortcut(area_shortcut, |_app, _shortcut, _event| {
+    if let Err(e) = app.global_shortcut().on_shortcut(area_shortcut, |_app, _shortcut, _event: ShortcutState| {
         info!("Hotkey triggered: area screenshot (Ctrl+Shift+S)");
         // 发送截屏事件到前端
         if let Err(e) = _app.emit("screenshot:trigger", serde_json::json!({"mode": "area"})) {
@@ -22,7 +22,7 @@ pub fn register_hotkeys(app: &AppHandle) -> Result<(), Box<dyn std::error::Error
 
     // Ctrl+Shift+A: 全屏截屏
     let full_shortcut = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyA);
-    if let Err(e) = app.global_shortcut().on_shortcut(full_shortcut, |_app, _shortcut, _event| {
+    if let Err(e) = app.global_shortcut().on_shortcut(full_shortcut, |_app, _shortcut, _event: ShortcutState| {
         info!("Hotkey triggered: full screenshot (Ctrl+Shift+A)");
         if let Err(e) = _app.emit("screenshot:trigger", serde_json::json!({"mode": "full"})) {
             error!("Failed to emit screenshot:trigger event: {}", e);
@@ -33,7 +33,7 @@ pub fn register_hotkeys(app: &AppHandle) -> Result<(), Box<dyn std::error::Error
 
     // Ctrl+Shift+W: 窗口截屏
     let window_shortcut = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::KeyW);
-    if let Err(e) = app.global_shortcut().on_shortcut(window_shortcut, |_app, _shortcut, _event| {
+    if let Err(e) = app.global_shortcut().on_shortcut(window_shortcut, |_app, _shortcut, _event: ShortcutState| {
         info!("Hotkey triggered: window screenshot (Ctrl+Shift+W)");
         if let Err(e) = _app.emit("screenshot:trigger", serde_json::json!({"mode": "window"})) {
             error!("Failed to emit screenshot:trigger event: {}", e);
